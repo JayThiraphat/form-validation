d02d5cb0a1753402703081f8b0e6a396bab361da
JayThiraphat <wongwatnicha@gmail.com>
2023-02-25 04:03:47 +0700

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import FormComponent from './components/FormComponent'
+
+function App() {
+
+  return (
+    <div className="App">
+     <FormComponent/>
+      
+    </div>
+  )
+}
+
+export default App
